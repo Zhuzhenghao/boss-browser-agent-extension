57cf8cdaed5275bb9c65e7dd5127e270e648b3f6
Zhuzhenghao <hezier.zzh@gmail.com>
2026-04-19 17:21:24 +0800

    chore: reindex files with updated gitignore

--- packages/extension/entrypoints/background.js
+++
@@ -1,7 +0,0 @@
-import { defineBackground } from 'wxt/utils/define-background';
-
-export default defineBackground(() => {
-  chrome.runtime.onInstalled.addListener(async () => {
-    await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
-  });
-});
--- packages/server/src/server/http.js
+++
@@ -1,3 +0,0 @@
-export function sendJson(res, statusCode, payload) {
-  res.status(statusCode).json(payload);
-}
